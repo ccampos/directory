f1063b16f6ee9744b495895eedcf53a49c04fa89
Claudio Campos <claudio@claudiocampos.com>
2013-02-22 17:45:32 -0500

    first commit

---
+++ js/generator.js
@@ -0,0 +1,11 @@
+var alphabet = 'abcdefghijfklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ#_.';
+
+alphabet = alphabet.split('');
+
+for (var i = 0; i < 5; i += 1) {
+  for (var j = 0, max = alphabet.length; j < 64; j += 1) {
+    word.push(alphabet[Math.round(Math.random() * max)]);
+  }
+  console.log(word.join(''));
+  word = [];
+};
